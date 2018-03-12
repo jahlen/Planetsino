46d2591bbe28af70e934d33cabadfb3beac24d8c
Johan Åhlén <johan@ahlen.name>
2018-03-12 17:26:23 +0100

    Wait for database creation to complete

--- Planetsino/Global.asax.cs
+++ Planetsino/Global.asax.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -12,23 +13,43 @@ namespace Planetsino
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        public static TaskCompletionSource<Exception> DatabaseReady = new TaskCompletionSource<Exception>();
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-            InitDatabase();
+            Task.Run(InitDatabase);
+
+            // Make sure database and collection is created before continuing
+            var dbCompletedInitialization = DatabaseReady.Task.Wait(TimeSpan.FromSeconds(60));
+            if (!dbCompletedInitialization)
+                throw new Exception("Database initialization timed out");
+
+            // Check if an exception occured during initialization
+            if (DatabaseReady.Task.Result != null)
+                throw DatabaseReady.Task.Result;
         }
 
-        private async void InitDatabase()
+        private async Task InitDatabase()
         {
-            await DbHelper.CreateDatabases();
-            await DbHelper.CreateCollections(Player.CollectionId, Player.PartitionKey);
+            try
+            {
+                await DbHelper.CreateDatabases();
+                await DbHelper.CreateCollections(Player.CollectionId, Player.PartitionKey);
 
-            var path = HttpRuntime.AppDomainAppPath; // Server.MapPath("~");
-            var procedureBody = File.ReadAllText(Path.Combine(path, "Procedures/AdjustBalance.js"));
-            await DbHelper.CreateStoredProcedure(DbHelper.PrimaryClient.Name, "adjustBalance", Player.CollectionId, procedureBody);
+                var path = HttpRuntime.AppDomainAppPath; // Server.MapPath("~");
+                var procedureBody = File.ReadAllText(Path.Combine(path, "Procedures/AdjustBalance.js"));
+                await DbHelper.CreateStoredProcedure(DbHelper.PrimaryClient.Name, "adjustBalance", Player.CollectionId, procedureBody);
+
+                DatabaseReady.SetResult(null);
+            }
+            catch (Exception ex)
+            {
+                DatabaseReady.SetResult(ex);
+            }
         }
     }
 }
